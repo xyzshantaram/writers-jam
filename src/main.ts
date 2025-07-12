71d70342fda6dd2718a87fc8ce57b9998398489f
Siddharth Singh <me@shantaram.xyz>
2025-07-13 01:00:21 +0530

    create createApp

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,8 @@ import * as path from "@std/path";
 import { makeCors, makeLimiter } from "./utils/middleware.ts";
 import { Liquid } from "liquidjs";
 import { config } from "./config.ts";
-
-const app = express();
-app.use(express.static("./public"));
-const liquid = new Liquid({ extname: ".liquid" });
-
-app.engine("liquid", liquid.express());
-app.set("view engine", "liquid");
-app.set("views", path.resolve("./templates"));
-app.use(express.static("./public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(makeCors());
-
-const limiter6pm = makeLimiter(6);
-const limiter2pm = makeLimiter(2);
+import { createPostSchema } from "./schemas/mod.ts";
+import { errorHandler } from "./error.ts";
 
 /*
 POST /post
@@ -40,13 +28,40 @@ GET /admin/reports
 GET /admin/reports/:lid
 POST /admin/posts/:ulid/delete
 POST /admin/posts/:ulid/reset-reports
+POST /admin/posts/:ulid/comment/:ulid/delete
 POST /admin/post/:ulid/mark-safe
 */
 
-app.post("/posts", (req, res) => {
-  res.send(req.body);
-});
+const createApp = () => {
+  const app = express();
+  app.use(express.static("./public"));
+  const liquid = new Liquid({ extname: ".liquid" });
 
-app.listen(config.httpPort).on("listening", () => {
+  app.engine("liquid", liquid.express());
+  app.set("view engine", "liquid");
+  app.set("views", path.resolve("./templates"));
+  app.use(express.static("./public"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(makeCors());
+
+  const limiter6pm = makeLimiter(6);
+  const limiter2pm = makeLimiter(2);
+
+  app.all("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.post("/post", (req, res) => {
+    const parsed = createPostSchema.parse(req.body);
+    console.log(parsed);
+    res.send(req.body);
+  });
+
+  app.use(errorHandler);
+
+  return app;
+};
+
+createApp().listen(config.httpPort).on("listening", () => {
   console.log(`Listening at http://0.0.0.0:${config.httpPort}`);
 });
