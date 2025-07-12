4cfa8da723db91aefec82fdc9da9a4109d3aac7a
Siddharth Singh <me@shantaram.xyz>
2025-07-13 01:30:12 +0530

    create getPost function

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,11 @@
 import { DatabaseSync } from "node:sqlite";
-import z from "zod";
-import { createCommentSchema, createPostSchema } from "./schemas/mod.ts";
+import { z } from "zod/v4";
+import { createCommentSchema, createPostSchema, Post } from "./schemas/mod.ts";
 import { ulid } from "@std/ulid";
 
 const db = new DatabaseSync("./writing-jam.db", {
   enableForeignKeyConstraints: true,
+  readOnly: false,
 });
 
 db.exec(`\
@@ -25,11 +26,11 @@ CREATE TABLE IF NOT EXISTS post (
 db.exec(`\
 CREATE TABLE IF NOT EXISTS comment (
   id TEXT PRIMARY KEY,
-  on TEXT NOT NULL,
+  for TEXT NOT NULL,
   content TEXT NOT NULL,
   nickname TEXT,
   created INTEGER NOT NULL,
-  FOREIGN KEY (on) REFERENCES post(id) ON DELETE CASCADE
+  FOREIGN KEY (for) REFERENCES post(id) ON DELETE CASCADE
 )`);
 
 const createPostStmt = db.prepare(`INSERT INTO post (
@@ -39,7 +40,7 @@ const createPostStmt = db.prepare(`INSERT INTO post (
     password,
     triggers,
     title,
-    nickname,
+    nickname
   ) VALUES (
     :id,
     :content,
@@ -47,7 +48,7 @@ const createPostStmt = db.prepare(`INSERT INTO post (
     :password,
     :triggers,
     :title,
-    :nickname,
+    :nickname
   )`);
 
 export const createPost = (opts: z.infer<typeof createPostSchema>) => {
@@ -61,16 +62,63 @@ export const createPost = (opts: z.infer<typeof createPostSchema>) => {
   return id;
 };
 
+interface GetPostOpts {
+  sort?: "views" | "updated";
+  nsfw?: "yes" | "no";
+}
+
+export const getPosts = (
+  opts?: GetPostOpts,
+): Omit<Post, "content" | "reports" | "deleted">[] => {
+  const { sort, nsfw } = opts || {};
+  let query =
+    `SELECT id, title, nsfw, password, triggers, nickname, updated, views FROM post`;
+  const conditions: string[] = [
+    "deleted != 1",
+  ];
+  const params: Record<string, any> = {};
+
+  if (nsfw === "yes") {
+    conditions.push("nsfw = 1");
+  } else if (nsfw === "no") {
+    conditions.push("nsfw = 0");
+  }
+
+  if (conditions.length) {
+    query += " WHERE " + conditions.join(" AND ");
+  }
+
+  if (sort === "views") {
+    query += " ORDER BY views DESC";
+  } else {
+    query += " ORDER BY updated DESC";
+  }
+
+  const stmt = db.prepare(query);
+
+  return stmt.all(params)
+    .map((row) => ({
+      id: row.id as string,
+      title: row.title as string,
+      nsfw: !!row.nsfw,
+      password: row.password as string | undefined,
+      triggers: row.triggers as string | undefined,
+      nickname: row.nickname as string,
+      updated: Number(row.updated),
+      views: Number(row.views),
+    }));
+};
+
 const createCommentStmt = db.prepare(`\
 INSERT INTO comment(
     id,
-    on,
+    for,
     content,
     nickname,
     created
 ) VALUES (
     :id,
-    :on,
+    :for,
     :content,
     :nickname,
     :created
