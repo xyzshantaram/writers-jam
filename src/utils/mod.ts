fc3ee9832ab40ca5a825b06de3f4cd954d52b491
Siddharth Singh <me@shantaram.xyz>
2025-07-13 06:19:03 +0530

    add choose function

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -4,3 +4,7 @@ export const die = (code: number, ...args: any[]): never => {
 };
 
 export const fatal = (...args: any[]) => die(1, "fatal:", ...args);
+
+export const choose = (arr: any[]) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
