69be8ea562c59c7a6e5872d7366bf521e47ace30
Siddharth Singh <me@shantaram.xyz>
2025-07-17 22:27:59 +0530

    add captcha routes

---
+++ src/routes/captcha.ts
@@ -0,0 +1,18 @@
+import Cap from "@cap.js/server";
+import { Request, Response } from "express";
+
+const cap = new Cap({
+  tokens_store_path: ".data/cap-state.json",
+});
+
+export const challenge = (_: Request, res: Response) => {
+  res.json(cap.createChallenge());
+};
+
+export const redeem = async (req: Request, res: Response) => {
+  const { token, solutions } = req.body;
+  if (!token || !solutions) {
+    return res.status(400).json({ success: false });
+  }
+  res.json(await cap.redeemChallenge({ token, solutions }));
+};
