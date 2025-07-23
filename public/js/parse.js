8847e203b1c2bdd05393453518bb5a65247782d0
Siddharth Singh <me@shantaram.xyz>
2025-07-23 23:53:55 +0530

    split off md parser to diff file

---
+++ public/js/parse.js
@@ -0,0 +1,24 @@
+import { marked } from "https://esm.sh/marked@^16.0.0";
+import { markedSmartypants } from "https://esm.sh/marked-smartypants@1.1.10";
+import sanitize from "https://esm.sh/sanitize-html@^2.17.0";
+
+const renderer = {
+    heading({ tokens, depth }) {
+        const text = this.parser.parseInline(tokens);
+        return `<div class='heading-${depth}'>${text}</div>`;
+    },
+    image() {
+        return '';
+    }
+};
+
+marked.use(markedSmartypants());
+marked.use({ renderer });
+
+export function parseMd(markdown) {
+    return sanitize(marked.parse(markdown), {
+        allowedClasses: {
+            'div': ['heading-1', 'heading-2', 'heading-3', 'heading-4', 'heading-5', 'heading-6']
+        }
+    });
+}
