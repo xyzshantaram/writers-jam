08de4df28f8c2a580d1435a766297e0ab5bd3a5b
Siddharth Singh <me@shantaram.xyz>
2025-08-12 00:37:53 +0530

    move isAdmin to middleware.ts

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,9 +1,10 @@
 import cors from "cors";
 import rateLimit from "express-rate-limit";
-import express from "express";
+import { NextFunction, Request, Response } from "express";
 import { errors } from "../error.ts";
 import { RateLimited } from "../errors/general.ts";
-import { getClientIP } from "./mod.ts";
+import { extractTokenFromHeader, getClientIP, verifyToken } from "./mod.ts";
+import { InvalidToken, MissingToken } from "../errors/admin.ts";
 
 export const makeCors = () =>
     cors({
@@ -13,7 +14,7 @@ export const makeCors = () =>
 
 export const makeLimiter = (reqs: number, duration: number) =>
     rateLimit({
-        keyGenerator: (req: express.Request) => {
+        keyGenerator: (req: Request) => {
             const firstPart = (s: string, sep: string, n = 0) => s.split(sep).at(n)?.trim()!;
             const clientIP = getClientIP(req);
             const path = firstPart(req.originalUrl, "?");
@@ -26,3 +27,16 @@ export const makeLimiter = (reqs: number, duration: number) =>
         windowMs: duration,
         limit: reqs,
     });
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    const token = extractTokenFromHeader(authHeader);
+    if (!token) return errors.json(res, ...MissingToken);
+
+    try {
+        verifyToken(token);
+        next();
+    } catch (_) {
+        return errors.json(res, ...InvalidToken);
+    }
+};
