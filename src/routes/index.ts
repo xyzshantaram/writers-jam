f0f2dd65f52d2582ac0415472fe069ca28a7deef
Siddharth Singh <me@shantaram.xyz>
2026-01-02 01:09:13 +0530

    homepage: display current edition, allow browsing all editions, add feed subtitles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
-import { getHomepageFeeds, getPostCount, getViewCount } from "../db/mod.ts";
+import {
+    getAllEditions,
+    getCurrentEdition,
+    getHomepageFeeds,
+    getPostCount,
+    getViewCount,
+} from "../db/mod.ts";
 import description from "../../data/description.md" with { type: "text" };
 import { config } from "../config.ts";
 
@@ -16,6 +22,16 @@ export const getDescription = () => {
     return result;
 };
 
+const getCurrentEditionInfo = () => {
+    const all = getAllEditions();
+    const id = getCurrentEdition();
+    const item = all.find((i) => i.id === id);
+    const name = item?.name || "Unknown";
+    const desc = item?.description || "";
+
+    return { id, name, desc };
+};
+
 export const get = (_: Request, res: Response) => {
     const postCount = getPostCount();
     const viewCount = getViewCount();
@@ -26,5 +42,6 @@ export const get = (_: Request, res: Response) => {
         viewCount,
         description: getDescription(),
         feeds: getHomepageFeeds(),
+        edition: getCurrentEditionInfo(),
     });
 };
